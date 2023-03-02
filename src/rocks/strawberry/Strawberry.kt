9caca6bd635c641569106ef9737eef2d9a11cace
Koudai Aono <koxudaxi@gmail.com>
2023-03-03 00:36:51 +0900

    Reduce unnecessary resolve in type providers

--- src/rocks/strawberry/Strawberry.kt
+++ src/rocks/strawberry/Strawberry.kt
@@ -1,51 +1,15 @@
 package rocks.strawberry
 
-import com.intellij.psi.PsiElement
-import com.intellij.psi.ResolveResult
 import com.intellij.psi.util.QualifiedName
-import com.jetbrains.python.psi.PyClass
-import com.jetbrains.python.psi.PyDecoratable
-import com.jetbrains.python.psi.PyReferenceExpression
-import com.jetbrains.python.psi.PyUtil
-import com.jetbrains.python.psi.resolve.PyResolveContext
-import com.jetbrains.python.psi.resolve.PyResolveUtil
-import com.jetbrains.python.psi.types.PyClassType
-import com.jetbrains.python.psi.types.PyType
-import com.jetbrains.python.psi.types.PyUnionType
-import com.jetbrains.python.psi.types.TypeEvalContext
+import com.jetbrains.python.psi.*
 
-val DATACLASS_QUALIFIED_NAME = QualifiedName.fromDottedString("strawberry.type")
+const val DATACLASS_SHORT_NAME = "strawberry.type"
+const val DATACLASS_LONG_NAME = "strawberry.object_type.type"
 
+val DATACLASS_NAMES = listOf(
+    DATACLASS_SHORT_NAME,
+    DATACLASS_LONG_NAME
+)
 
-fun hasDecorator(pyDecoratable: PyDecoratable, refName: QualifiedName): Boolean {
-    return pyDecoratable.decoratorList?.decorators?.mapNotNull { it.callee as? PyReferenceExpression }?.any {
-        PyResolveUtil.resolveImportedElementQNameLocally(it).any { decoratorQualifiedName ->
-            decoratorQualifiedName == refName
-        }
-    } ?: false
-}
-
-
-fun isDataclass(pyClass: PyClass): Boolean {
-    return hasDecorator(pyClass, DATACLASS_QUALIFIED_NAME)
-}
-
-fun getResolveElements(referenceExpression: PyReferenceExpression, context: TypeEvalContext): Array<ResolveResult> {
-    return PyResolveContext.defaultContext(context).let {
-        referenceExpression.getReference(it).multiResolve(false)
-    }
-
-}
-
-fun getResolvedPsiElements(referenceExpression: PyReferenceExpression, context: TypeEvalContext): List<PsiElement> {
-    return getResolveElements(referenceExpression, context).let { PyUtil.filterTopPriorityResults(it) }
-}
-
-
-fun getPyClassTypeByPyTypes(pyType: PyType): List<PyClassType> {
-    return when (pyType) {
-        is PyUnionType -> pyType.members.mapNotNull { it }.flatMap { getPyClassTypeByPyTypes(it) }
-        is PyClassType -> listOf(pyType)
-        else -> listOf()
-    }
-}
+val DATACLASS_QUALIFIED_NAME = QualifiedName.fromDottedString(DATACLASS_SHORT_NAME)
+internal val PyFunction.isDataclass: Boolean get() = qualifiedName in DATACLASS_NAMES
